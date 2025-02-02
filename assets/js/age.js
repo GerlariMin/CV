00874cf6840355727d97e24521f78efb9e21d6d9
GerlariMin <hornitzaile@hotmail.fr>
2025-02-02 21:07:05 +0100

    V1.1.0: - Mise à jour des dépendances utilisées - Retrait des dépendances inutiles - Remplacement de JQuery par du JS Vanilla - Reprise graphique légère avec Bootstrap 5.3 - Actualisation des informations

--- assets/js/age.js
+++ assets/js/age.js
@@ -1,7 +1,11 @@
-// On attend que la page se charge
-$(document).ready(function() {
-    // Âge arrondi à l'année = date courant - année de naissance
-    let ageActuel = new Date().getFullYear() - 1997;
-    // On ajoute l'âge au span dédié contenant la classe age dans la barre de navigation
-    $('.age').text('(' + ageActuel + ' ans)');
+// On attend le chargement de la page HTML
+document.addEventListener("DOMContentLoaded", async function () {
+    // Récupération de l'année courante
+    let dateCourante = new Date();
+    // Âge arrondi = année courante - année de naissance
+    let ageActuel = dateCourante.getFullYear() - 1997;
+    // On calcule si on est avant le mois d'Avril de l'année courante ou après, pour avoir l'âge approximatif
+    if (dateCourante < new Date(dateCourante.getFullYear(), 3, 1)) {ageActuel--;}
+    // On ajoute l'âge au span dédié contenant l'id age dans la barre de navigation
+    document.getElementById('age').textContent = '(' + ageActuel + ' ans)';
 });
--- assets/js/anneeCopyright.js
+++ assets/js/anneeCopyright.js
@@ -1,11 +1,11 @@
-// On attend que la page se charge
-$(document).ready(function() {
+// On attend le chargement de la page HTML
+document.addEventListener("DOMContentLoaded", async function () {
     // Année de publication du site
     let anneePublication = 2022;
     // On récupère l'année courante
     let anneeCourante = new Date().getFullYear();
-    // Si l'année courante est supérieure à l'année de publication d'origine, on l'affiche dans le span du pieds de page dédié
+    // Si l'année courante est supérieure à l'année de publication d'origine, on l'affiche dans le span du pieds de page dédié ayant l'id anneeCopyright
     if(anneeCourante > anneePublication) {
-        $('.anneeCopyright').text(' - ' + anneeCourante);
+        document.getElementById('anneeCopyright').textContent = ' - ' + anneeCourante;
     }
 });
